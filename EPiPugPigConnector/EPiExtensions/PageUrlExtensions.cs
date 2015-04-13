374d0770d53f3b6a73b8d710c546d97816faa695
Arnold Macauley <github@2622.co.uk>
2015-04-13 11:13:50 +0100

    updated EPiServer Version to 8.3.0.0

--- EPiPugPigConnector/EPiExtensions/PageUrlExtensions.cs
+++ EPiPugPigConnector/EPiExtensions/PageUrlExtensions.cs
@@ -12,28 +12,28 @@ namespace EPiPugPigConnector.EPiExtensions
 {
     public static class PageUrlExtensions
     {
-        //This method is provided for situations where you want a clean external URL, even when in CMS edit mode
-        // e.g. jQuery, Form actions, etc.
-        private static string GetForcedExternalUrl(PageReference pageLink, string lang)
-        {
-            RequestContext requestContext = new RequestContext();
+        ////This method is provided for situations where you want a clean external URL, even when in CMS edit mode
+        //// e.g. jQuery, Form actions, etc.
+        //private static string GetForcedExternalUrl(PageReference pageLink, string lang)
+        //{
+        //    RequestContext requestContext = new RequestContext();
 
-            requestContext.RouteData = new RouteData();
-            requestContext.RouteData.DataTokens.Add("contextmode", ContextMode.Default);
+        //    requestContext.RouteData = new RouteData();
+        //    requestContext.RouteData.DataTokens.Add("contextmode", ContextMode.Default);
 
-            var routeValueDictionary = new RouteValueDictionary();
-            var urlResolver = new UrlResolver();
+        //    var routeValueDictionary = new RouteValueDictionary();
+        //    var urlResolver = new UrlResolver();
 
-            //temporarily set the request context to null
-            var contextSaved = HttpContext.Current;
-            HttpContext.Current = null;
+        //    //temporarily set the request context to null
+        //    var contextSaved = HttpContext.Current;
+        //    HttpContext.Current = null;
 
-            var url = urlResolver.GetVirtualPath(new ContentReference(pageLink.ID), lang, routeValueDictionary, requestContext);
+        //    var url = urlResolver.GetVirtualPath(new ContentReference(pageLink.ID), lang, routeValueDictionary, requestContext);
 
-            //now set it back
-            HttpContext.Current = contextSaved;
+        //    //now set it back
+        //    HttpContext.Current = contextSaved;
 
-            return url.GetUrl();
-        }
+        //    return url.GetUrl();
+        //}
     }
 }
